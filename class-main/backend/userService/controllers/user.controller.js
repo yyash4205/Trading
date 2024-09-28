fe4df7a77988ccd1815990b234a916bcfe0f764b
yyash4205 <166504178+yyash4205@users.noreply.github.com>
2024-09-28 16:32:01 +0530

    Update user.controller.js

--- class-main/backend/userService/controllers/user.controller.js
+++ class-main/backend/userService/controllers/user.controller.js
@@ -4,18 +4,22 @@ const registerUser = async (req, res) => {
   try {
     console.log('User Details:', req.body);
 
+    // Check if a user with the same email already exists
     const existingUser = await User.findOne({ email: req.body.email });
     
     if (existingUser) {
+      // If the email is already registered, send a response
       return res.status(400).send('Email is already registered');
     }
 
+    // Create a new user object with the details from the request body
     let userData = new User({
       username: req.body.username,
       email: req.body.email,
       password: req.body.password
     });
 
+    // Save the user data to the database
     await userData.save();
     
     res.send('User registered!!!');
